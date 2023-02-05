4a2f7efb2d87133ca3b28aeb31632de0cd468fd1
Tony Spataro <github.com@tracker.xeger.net>
2023-02-04 19:05:32 -0800

    Adapt for YubiKey

--- yubikey.py
+++ yubikey.py
@@ -1,11 +1,14 @@
 import argparse
-import re
+import colorama
 
-from subprocess import run, CalledProcessError, PIPE, DEVNULL
+from subprocess import Popen, CalledProcessError, PIPE
 
 from awsume.awsumepy import hookimpl, safe_print
 from awsume.awsumepy.lib import profile
 from awsume.awsumepy.lib.logger import logger
+
+# we print only N lines of output to minimize ykman stack trace spam
+MAX_OUTPUT_LINES = 2
 
 
 def find_item(config, mfa_serial):
@@ -31,25 +34,34 @@ def get_mfa_serial(profiles, target_name):
 
 
 def describe_failure(stderr):
-    # [ERROR] 2023/02/04 16:29:52 => 28 characters
-    return stderr.decode().strip('\n')[28:]
+    return stderr.decode().strip('\n')
 
 
 def get_otp(title):
     try:
-        op = run(['op', 'item', 'get', '--otp', title],
-                 check=True, stdout=PIPE, stderr=PIPE)
-        return op.stdout.decode().strip('\n')
-    except CalledProcessError as e:
-        logger.error('Failed: `op` command -> %s' %
-                     (describe_failure(e.stderr)))
-        return None
+        yk = Popen(['ykman', 'oath', 'accounts', 'code', '--single', title],
+                   stdout=PIPE, stderr=PIPE)
+        linecount = 0
+        while True:
+            msg = yk.stderr.readline().decode()
+            if msg == '' and yk.poll() is not None:
+                break
+            elif msg != '':
+                if linecount < MAX_OUTPUT_LINES:
+                    safe_print(msg, colorama.Fore.CYAN, end='')
+                    linecount += 1
+                else:
+                    logger.debug(msg.strip('\n'))
+
+        if yk.returncode != 0:
+            return None
+        return yk.stdout.readline().decode().strip('\n')
     except FileNotFoundError:
-        logger.error('Failed: missing `op` command')
+        logger.error('Failed: missing `ykman` command')
         return None
 
 
-@hookimpl
+@ hookimpl
 def pre_get_credentials(config: dict, arguments: argparse.Namespace, profiles: dict):
     mfa_serial = get_mfa_serial(profiles, arguments.target_profile_name)
     if mfa_serial:
