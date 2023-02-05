1f73755e8a0e5a43129c5025ca8cd0f32241734c
Tony Spataro <github.com@tracker.xeger.net>
2023-02-04 20:50:16 -0800

    v1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='awsume-yubikey-plugin',
-    version='1.0.1',
+    version='1.0.2',
     description='Automates awsume MFA entry via YubiKey CLI.',
     entry_points={
         'awsume': [
