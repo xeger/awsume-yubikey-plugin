8e96d41b6e8658c385d97565f0a627ce54642c7a
Tony Spataro <github.com@tracker.xeger.net>
2024-05-23 19:55:11 -0700

    v1.2.4

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='awsume-yubikey-plugin',
-    version='1.2.3',
-    description='Automates awsume MFA entry via YubiKey CLI.',
-    entry_points={
-        'awsume': [
-            'yubikey = yubikey'
-        ]
-    },
-    author='Tony Spataro',
-    author_email='pypi@tracker.xeger.net',
-    url='https://github.com/xeger/awsume-yubikey-plugin',
-    py_modules=['yubikey'],
+    name="awsume-yubikey-plugin",
+    version="1.2.4",
+    description="Automates awsume MFA entry via YubiKey CLI.",
+    entry_points={"awsume": ["yubikey = yubikey"]},
+    author="Tony Spataro",
+    author_email="pypi@tracker.xeger.net",
+    url="https://github.com/xeger/awsume-yubikey-plugin",
+    py_modules=["yubikey"],
 )
